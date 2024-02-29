7dd45d5baa91f0be1b90b86406ca00bf630ad5b5
Prince <43872697+prince475@users.noreply.github.com>
2024-02-29 14:32:53 +0300

    Finishing up the sellWeapon function

--- script.js
+++ script.js
@@ -124,7 +124,11 @@ function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
-
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
