fb82f461fc51d6ca3a9f612b2141c8eb636fbb99
Prince <43872697+prince475@users.noreply.github.com>
2024-03-01 17:21:19 +0300

    Adding the logic that happens when the monster is defeated.

--- script.js
+++ script.js
@@ -209,7 +209,11 @@ function dodge() {
 }
 
 function defeatMonster() {
-
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
 }
 
 function lose() {
