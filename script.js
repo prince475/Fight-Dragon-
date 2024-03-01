f4707aab9c8d2f237eeabdda5dcd8aa8cb6c6058
Prince <43872697+prince475@users.noreply.github.com>
2024-03-01 17:26:08 +0300

    Adding new object to the locations array

--- script.js
+++ script.js
@@ -72,6 +72,12 @@ const locations = [
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster."
+  },
+  {
+    name: "kill monster",
+    "button text": ["Go to town square", "Go to town square", "Go to town square"],
+    "button functions": [goTown, goTown, goTown],
+    text: "The monster screams Arg! as it dies. You gain experience points and find gold."
   }
 ];
 
