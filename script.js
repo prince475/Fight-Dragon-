44783d59624ee759ffde1674a884133ea4290f52
Prince <43872697+prince475@users.noreply.github.com>
2024-03-01 17:06:21 +0300

    finishing up the attack function

--- script.js
+++ script.js
@@ -194,6 +194,13 @@ function attack() {
    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
    health -= monsters[fighting].level;
    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+   healthText.innerText = health;
+   monsterHealthText.innerText = monsterHealth;
+   if (health <= 0) {
+     lose();
+   } else if (monsterHealth <= 0) {
+     defeatMonster();
+   }
 }
 
 function dodge() {
