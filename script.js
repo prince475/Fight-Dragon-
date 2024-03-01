41c6ac5b7b1dc8671921ee8ff9d8ee60eca4ce07
Prince <43872697+prince475@users.noreply.github.com>
2024-03-01 17:27:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ const locations = [
     name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
     "button functions": [goTown, goTown, goTown],
-    text: "The monster screams Arg! as it dies. You gain experience points and find gold."
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   }
 ];
 
