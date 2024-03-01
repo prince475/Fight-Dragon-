47f671f7c6614719d20e5c2a488dcf143ed07118
Prince <43872697+prince475@users.noreply.github.com>
2024-03-01 12:45:43 +0300

    Changing the display of monsterStats to 'block' and updating innerText of monsterName and monsterHealthText

--- script.js
+++ script.js
@@ -182,6 +182,9 @@ function fightDragon() {
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 // Adding the fighting techniques function
