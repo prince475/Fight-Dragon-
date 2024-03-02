cca2cd7324f5c337f6066a59a27e5cc0d7fec524
Prince <43872697+prince475@users.noreply.github.com>
2024-03-02 20:33:11 +0300

    Updating locations array

--- script.js
+++ script.js
@@ -78,6 +78,12 @@ const locations = [
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
     "button functions": [goTown, goTown, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;"
   }
 ];
 
