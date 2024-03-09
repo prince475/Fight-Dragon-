27870ab72aca1cfa5aecaad843402559f8685d05
Prince <43872697+prince475@users.noreply.github.com>
2024-03-09 22:48:20 +0300

    Adding an easter object to our array.

--- script.js
+++ script.js
@@ -90,6 +90,12 @@ const locations = [
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
     "button functions": [restart, restart, restart], 
     text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
   }
 ];
 
